ea70a7bbaccd7027e0a3d5964e29b4ad0ae1f918
swl <subluekey@gmail.com>
2024-08-06 13:57:48 +0800

    搭建项目

---
+++ src/main/java/com/yulu/YuLuMainApplication.java
@@ -0,0 +1,12 @@
+package com.yulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuLuMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YuLuMainApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  YuLu Run Success   ლ(´ڡ`ლ)ﾞ");
+    }
+}
